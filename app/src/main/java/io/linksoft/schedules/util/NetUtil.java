a1adf5667e802d16b2d6031e706c87e6c61cc0fd
Leon Kolthoff <leonkolthoff@gmail.com>
2017-09-11 23:51:41 +0200

    WiFi only schedule syncing

--- app/src/main/java/io/linksoft/schedules/util/NetUtil.java
+++ app/src/main/java/io/linksoft/schedules/util/NetUtil.java
@@ -2,14 +2,22 @@ package io.linksoft.schedules.util;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+
+import io.linksoft.schedules.data.Settings;
 
 public class NetUtil {
 
     public static boolean hasNetworkConnection(Activity activity) {
         ConnectivityManager connMgr = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (PreferenceManager.getDefaultSharedPreferences(activity).getBoolean(Settings.PREF_WIFI_ONLY, false))
+            return networkInfo.getType() == ConnectivityManager.TYPE_WIFI;
 
         return networkInfo != null && networkInfo.isConnected();
     }
