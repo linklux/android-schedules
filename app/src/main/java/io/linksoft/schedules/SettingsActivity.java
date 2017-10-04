0c4a7a02b254122742bfa2f49a05697512e6c287
Leon Kolthoff <leonkolthoff@gmail.com>
2017-10-04 10:24:22 +0200

    Settings activity app bar | Closes #22

--- app/src/main/java/io/linksoft/schedules/SettingsActivity.java
+++ app/src/main/java/io/linksoft/schedules/SettingsActivity.java
@@ -2,6 +2,7 @@ package io.linksoft.schedules;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import io.linksoft.schedules.fragments.SettingsFragment;
 
@@ -11,10 +12,18 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.nav_action_settings);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         // Display the fragment as the main content.
         getFragmentManager()
             .beginTransaction()
-            .replace(android.R.id.content, new SettingsFragment())
+            .replace(R.id.content_settings, new SettingsFragment())
             .commit();
     }
 
